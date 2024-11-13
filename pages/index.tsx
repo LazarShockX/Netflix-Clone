8907b4672beb0843bab5ee6a06b4946e27f5db5c
LazarShockX <donaldbr12@gmail.com>
2024-11-13 23:46:58 +0100

    Added protected routes and profiles screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <div>
       <h1>Netflix Clone</h1>
+      <p className="text-white">Logged in as {user?.name}</p>
+      <button onClick={() => signOut()}>
+        Log out
+      </button>
     </div>
   );
 }
