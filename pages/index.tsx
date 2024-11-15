e82281c6622c1469edbd1113857564f9bde03392
LazarShockX <donaldbr12@gmail.com>
2024-11-15 16:31:03 +0100

    Added favorites and modified serverAuth and nextauth

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 
+import useFavorites from "@/hooks/useFavorites";
 import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -26,12 +27,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+
   return (
     <div>
       <Navbar />
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </div>
   );
