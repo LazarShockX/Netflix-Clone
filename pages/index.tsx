5d396d954fd348d16dda91f641b87072c815fc3b
LazarShockX <donaldbr12@gmail.com>
2024-11-15 12:27:39 +0100

    Added movie list and movie card

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,10 +25,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   return (
     <div>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </div>
   );
 }
